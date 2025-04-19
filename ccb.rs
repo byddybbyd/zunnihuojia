00d38fab8b69e0c15e3b11870ce87e23c7512b63
byddybbyd <byddybbyd@proton.me>
2025-04-19 13:46:46 +0800

    Add files via upload

---
+++ ccb.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!("   ccbccb      ccbccb   ccbccb");
+	println!("ccb         ccb         ccb   ccb");
+	println!("ccb         ccb         ccbccb");
+	println!("ccb         ccb         ccb   ccb");
+	println!("   ccbccb      ccbccb   ccbccb");
+} 
